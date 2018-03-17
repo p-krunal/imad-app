722884e3beeb1472be28b589416bcfe5e925e1ae
p-krunal <37190579+p-krunal@users.noreply.github.com>
2018-03-17 11:28:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,9 +74,9 @@ app.get('/', function (req, res) {
 });
 
 app.get('/counter', function (req,res) {
-    counter = counter+1;
+    counter = counter + 1;
     res.send(counter.toString());
-})
+});
 app.get('/:articleName', function(req,res){
     //articelName == articele-one
     //articles[articleName] ==  {} content for object articel one
